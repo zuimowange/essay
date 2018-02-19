b852968d3393d789ee2f164be2cdab6f155927d6
wangege <1433166170@qq.com>
2018-02-19 22:06:20 +0800

    修改登录界面

--- src/main/resources/static/js/user/login.js
+++ src/main/resources/static/js/user/login.js
@@ -1,3 +1,21 @@
+/**
+ * 登录方式切换
+ */
+$(".title_ul li").click(function(){
+	$(this).siblings().removeClass("choose_li")
+	$(this).addClass("choose_li")
+	$(".login_ul").hide();
+	$("."+ $(this).attr("tabindex")).show();
+})
+/**
+ * 二维码生成
+ */
+qrcodelib.toCanvas(document.getElementById('erWeiMao'), 'http://www.baidu.com', function(error) {
+	if(error) console.error(error)
+})
+/**
+ * 登录方法
+ */
 $("#loginBtn").click(function(){
 	var account = $("#account").val();
 	var pwd = $("#pwd").val();
@@ -10,7 +28,16 @@ $("#loginBtn").click(function(){
 		},
 		success:function(data){
 			CookieUtil.set("token",data.token);
-			alert(data.msg);
+			if(data.msgCode == 1){
+				layer.msg(data.msg, {icon: 6},function(){
+					location.href = "/user/register";
+				});
+			}
+			else{
+				layer.msg(data.msg, {icon: 5},function(){
+					location.href = "/user/register";
+				});
+			}
 		}
 	});
 })
--- src/main/resources/static/js/user/register.js
+++ src/main/resources/static/js/user/register.js
@@ -1,3 +1,15 @@
+/**
+ * 注册方式切换
+ */
+$(".title_ul li").click(function(){
+	$(this).siblings().removeClass("choose_li")
+	$(this).addClass("choose_li")
+	$(".login_ul").hide();
+	$("."+ $(this).attr("tabindex")).show();
+})
+/**
+ * 注册方法
+ */
 $("#loginBtn").click(function(){
 	var account = $("#account").val();
 	var pwd = $("#pwd").val();
